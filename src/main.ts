d900f8a51d414eb8655b59fea876226bfdcd275b
Carlos <carlosaac232001@gmail.com>
2025-08-11 09:56:23 -0500

    refactor(main): integrate header, products, and footer components into a solo component

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { productsListComponent } from './products.ts';
 import { footerComponent } from './footer.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const headerContainer = document.getElementById('header');
-  const productsContainer = document.getElementById('products');
-  const footerContainer = document.getElementById('footer');
-  headerContainer?.appendChild(headerComponent());
-  productsContainer?.appendChild(productsListComponent());
-  footerContainer?.appendChild(footerComponent());
+  const app = document.getElementById('app');
+  app?.appendChild(headerComponent());
+  app?.appendChild(productsListComponent());
+  app?.appendChild(footerComponent());
 });
