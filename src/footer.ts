10902bd16f16e8185892047650bbd138a73d7777
Carlos <carlosaac232001@gmail.com>
2025-08-11 09:49:06 -0500

    feat(footer): add footer component for app layout

---
+++ src/footer.ts
@@ -0,0 +1,6 @@
+export function footerComponent() {
+  const footer = document.createElement('footer');
+  footer.innerHTML = '<p>© 2025 Carlos Acosta</p>';
+  footer.classList.add('footer');
+  return footer;
+}
